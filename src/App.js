064e553248ef99b7ebfe4c4ce35341704a0977f3
Almaz <almazius11@gmail.com>
2023-03-24 12:12:49 +0300

    adding the game logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Board } from './components/Board'
 
 function App() {
-  const board = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [xPlaying, setXPlaying] = useState(true)
+
+  const handleBoxClick = (boxIdx) => {
+    const updatedBoard = board.map((value, idx) => {
+      if (idx === boxIdx) {
+        return xPlaying === true ? 'X' : 'O'
+      } else {
+        return value
+      }
+    })
+
+    setBoard(updatedBoard)
+
+    setXPlaying(!xPlaying)
+  }
+
   return (
     <div className="App">
-      <Board board={board} onClick={null} />
+      <Board board={board} onClick={handleBoxClick} />
     </div>
   )
 }
